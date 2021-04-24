698334a6ddf9bd5f0f7686761bb240a8b751a866
t-bendet <talbendet21@gmail.com>
2021-04-24 17:44:20 +0300

    s

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  console.log("prodocting");
+  console.log("prodoctin");
   module.exports = require("../config/prod");
 } else {
   module.exports = require("../config/dev");
