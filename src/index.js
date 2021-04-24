a89ba7d8b8639f93eafab0694eff6f6a3acc0225
t-bendet <talbendet21@gmail.com>
2021-04-24 19:19:06 +0300

    s

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const bankRouter = require("./routers/bank");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, "./index.js")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.js"));
+});
+
 app.use(express.json());
 app.use(bankRouter);
 app.use(cors);
-
-app.use(express.static(path.join(__dirname, "./index.js")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./index.js"));
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
