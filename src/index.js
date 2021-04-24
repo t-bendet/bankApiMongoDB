e127ec3fcfbe8d31fb3b5df1c7122f220baaf1ef
t-bendet <talbendet21@gmail.com>
2021-04-24 18:49:40 +0300

    s

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(bankRouter);
 app.use(cors);
 
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
